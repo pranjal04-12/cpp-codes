b9684b30f95cb5252744885cfc3a51c2d720146d
Pranjal Kathar <pranjalkathar@gmail.com>
2026-09-19 11:08:38 +0530

    Added new course work

---
+++ namespace.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+namespace myname{
+    void display(){
+        //code
+    }
+}
+int main() {
+    myname :: display();
+    cout << "Hello, World!" << endl;
+    
+    return 0;
+}
